bcf6f71920135bfc99e3d67c481416bf645bbada
tomppu144 <tomppu144@gmail.com>
2026-08-18 13:49:49 +0300

    lisättiin uusi rivi

--- pythonlol.py
+++ pythonlol.py
@@ -1,2 +1,16 @@
 print("Hello, World!")
 print("Okei, Okei")
+
+käyttäjä = input('Anna nimesi: ')
+
+
+print(f"Hauska tavata, {käyttäjä}!")
+
+
+kanta = float(input('Anna suorakulmion kannan pituus: '))
+korkeus = float(input('Anna suorakulmion korkeus: '))
+
+
+ala = kanta * korkeus
+
+print(f"Suorakulmion pinta-ala on {ala:.2f}")
